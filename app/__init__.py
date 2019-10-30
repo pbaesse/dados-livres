dfef63339345fd01283d6001bfdbb3188919e3fe
Carolina Soares <m.carolina.soares1@gmail.com>
2019-10-29 22:15:39 -0300

    Add configurações data e hora

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,9 @@ babel = Babel(app)
 def get_locale():
     # return request.accept_languages.best_match(app.config['LANGUAGES'])
     return 'en'
+
+from flask_moment import Moment
+moment = Moment(app)
     
 from app import routes, models
 
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,20 @@
 from app import app
 from flask_babel import _, get_locale
 
-@app.before_request
-def before_request():
-    # ...
-    g.locale = str(get_locale())
-
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!" #criar página principal
 
-from flask import render_template, flash, redirect, url_for, g
+from flask import render_template, flash, redirect, url_for, request, g
   
+@app.before_request
+def before_request():
+    #if current_user.is_authenticated:
+    #    current_user.last_seen = datetime.utcnow()
+    #    db.session.commit()
+    g.locale = str(get_locale())
+    
 from app.forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 def login():
