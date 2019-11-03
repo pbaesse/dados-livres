23bf368693b4911e6fa615c1cf4c7ccfc02160c9
Carolina Soares <m.carolina.soares1@gmail.com>
2019-11-02 21:51:55 -0300

    Rotas estão trabalhando com Login

--- app/routes.py
+++ app/routes.py
@@ -10,17 +10,26 @@ from flask import render_template, flash, redirect, url_for, request, g
   
 @app.before_request
 def before_request():
-    #if current_user.is_authenticated:
-    #    current_user.last_seen = datetime.utcnow()
-    #    db.session.commit()
     g.locale = str(get_locale())
-    
+
+from flask_login import current_user, login_user
+from app.models import User   
 from app.forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+    	return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(_('Login solicitado para o usuário {}, Lembre de mim={}').format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash(_('Nome de usuário ou senha inválidos'))
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html', title=_('Fazer Login'), form=form)
+    return render_template('login.html', title=_('Conecte-se'), form=form)
+    
+from flask_login import logout_user
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
