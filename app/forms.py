7ec0625b4a4d394ed8111ef13901a1213944ad7e
Carolina Soares <m.carolina.soares1@gmail.com>
2019-11-09 01:26:06 -0300

    Ajustei as internacionalização

--- app/forms.py
+++ app/forms.py
@@ -2,28 +2,28 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
-from flask_babel import _, lazy_gettext as _l
+from flask_babel import _
 
 class LoginForm(FlaskForm):
-    username = StringField(_l('Nome'), validators=[DataRequired()])
-    password = PasswordField(_l('Senha'), validators=[DataRequired()])
-    remember_me = BooleanField(_l('Lembre de mim'))
-    submit = SubmitField(_l('Entrar'))
+    username = StringField(_('Nome'), validators=[DataRequired()])
+    password = PasswordField(_('Senha'), validators=[DataRequired()])
+    remember_me = BooleanField(_('Lembre de mim'))
+    submit = SubmitField(_('Entrar'))
     
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    username = StringField(_('Nome'), validators=[DataRequired()])
+    email = StringField(_('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(_('Senha'), validators=[DataRequired()])
+    password2 = PasswordField(_(
+        'Repita a senha'), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(_('Registrar'))
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Please use a different username.')
+            raise ValidationError(_('Por favor, use um nome de usuário diferente.'))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Please use a different email address.')
+            raise ValidationError(_('Por favor, use um endereço de e-mail diferente.'))
--- app/routes.py
+++ app/routes.py
@@ -20,15 +20,15 @@ def before_request():
 def index():
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'autor': {'nome': 'Daniel'},
+            'mensagem': 'Que dia lindo em SP!'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'autor': {'nome': 'Paula'},
+            'mensagem': 'O filme dos Vingadore foi ótimo!'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title=_('Inicial'), posts=posts)
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,14 +38,14 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first() 
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash(_('Nome de usuário ou senha inválidos'))
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title=_('Entrar'), form=form)
     
 @app.route('/logout')
 def logout():
@@ -62,6 +62,6 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
+        flash(_('Parabéns, agora você é um usuário registrado!'))
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title=_('Registrar'), form=form)
