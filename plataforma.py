c453010639150f805d5b0ea15818a14acebfc2e0
Carolina Soares <m.carolina.soares1@gmail.com>
2019-11-14 21:22:52 -0300

    Configurei Flask shell em plataforma.py

--- plataforma.py
+++ plataforma.py
@@ -1,1 +1,6 @@
 from app import app, db, cli
+from app.models import User, Source, Software
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Source': Source, 'Software': Software}
