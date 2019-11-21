fead37e9460a1fadb33512f31ce7200165139e74
Carolina Soares <m.carolina.soares1@gmail.com>
2019-11-21 00:16:35 -0300

    Instalei flask-bootstap e iniciei em init.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_script import Manager
 from flask_login import LoginManager
 #from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ manager.add_command('db', MigrateCommand)
 login = LoginManager(app)
 #moment = Moment(app)
 babel = Babel(app)
+bootstrap = Bootstrap(app)
 
 @babel.localeselector
 def get_locale():
