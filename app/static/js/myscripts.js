c30098225f7f26a4d2061c53329e638ce10e2661
mariacarolinass <m.carolina.soares1@gmail.com>
2020-09-30 23:41:52 -0300

    apaguei função tag js em desuso

--- app/static/js/myscripts.js
+++ app/static/js/myscripts.js
@@ -43,21 +43,3 @@ $(document).ready(function () {
     source: titles
   });
 });
-
-
-$(document).ready(function () {
-  var tags = [
-
-  ];
-
-  function loadTags() {
-    $.getJSON('/_tag', function (data, status, xhr) {
-      for (var i = 0; i < data.length; i++) {
-        tags.push(data[i].tag);
-      }
-    });
-  };
-
-  loadTags();
-
-});
