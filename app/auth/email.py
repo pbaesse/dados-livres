253c84063db8c5f37a54804a67f77f970f4903b5
mariacarolinass <m.carolina.soares1@gmail.com>
2020-10-23 01:47:44 -0300

    add função de concluir inscrição por email

--- app/auth/email.py
+++ app/auth/email.py
@@ -11,3 +11,13 @@ def send_password_reset_email(user):
                                         user=user, token=token),
                 html_body=render_template('email/reset_password.html',
                                         user=user, token=token))
+
+def send_register_confirm_email(user):
+    token = user.get_confirm_register_token()
+    send_email(_('[Dados Livres] Conclua sua inscrição'),
+                sender=current_app.config['ADMINS'][0],
+                recipients=[user.email],
+                text_body=render_template('email/confirm_email.txt',
+                                        user=user, token=token),
+                html_body=render_template('email/confirm_email.html',
+                                        user=user, token=token))
