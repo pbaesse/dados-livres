be62547305258e3d4360c9d7156075a5bb74e643
Flávio Filipe <flaviofilipe.dev@gmail.com>
2020-10-27 18:18:15 -0300

    Criando env de exemplo e ocultando informacoes sensiveis

--- config.py
+++ config.py
@@ -6,23 +6,27 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'xdbq\x10\xf9\x11J6z\xee\xc4Y\x19oG[\x0c'
+
     RECAPTCHA_USE_SSL = False
-    RECAPTCHA_PUBLIC_KEY='you-will-never-guess'
-    RECAPTCHA_PRIVATE_KEY='you-will-never-guess'
-    RECAPTCHA_OPTIONS= {'theme':'black'}
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY') or 'you-will-never-guess'
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY') or'you-will-never-guess'
+    RECAPTCHA_OPTIONS = {'theme':'black'}
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BOOTSTRAP_SERVE_LOCAL = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') or False
+
+    BOOTSTRAP_SERVE_LOCAL = os.environ.get('BOOTSTRAP_SERVE_LOCAL') or True
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = 1
-    MAIL_USERNAME = 'dadoslivres.testes@gmail.com'
-    MAIL_PASSWORD = 'carolina.2019'
-    ADMINS = ['dadoslivres.testes@gmail.com']
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 587
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    ADMINS = os.environ.get('ADMINS').split(',')
     LANGUAGES = ['en', 'pt']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-    POSTS_PER_PAGE = 20
-    COMMENTS_PER_PAGE = 10
+    POSTS_PER_PAGE = os.environ.get('POSTS_PER_PAGE') or 20
+    COMMENTS_PER_PAGE = os.environ.get('COMMENTS_PER_PAGE') or 10
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
